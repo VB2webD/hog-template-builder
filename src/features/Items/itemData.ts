cdfba53cbfe193921bfd5933ba4116da5689d57c
volker.bransky <volker.bransky@check24.de>
2025-04-07 13:19:56 +0200

    Add items Assets

--- src/features/Items/itemData.ts
+++ src/features/Items/itemData.ts
@@ -1,6 +1,114 @@
 import {ItemEntity} from "../../entities/itemEntity.ts";
 
 export const Items: ItemEntity[] = [
+    // slow auras
+    {
+        name: "xx",
+        image: "/items/slow/slowT1.png",
+        tier: 1
+    }, {
+        name: "xx",
+        image: "/items/slow/slowT2.png",
+        tier: 2
+    }, {
+        name: "xx",
+        image: "/items/slow/slowT3.webp",
+        tier: 3
+    },
+    // - armor
+    {
+        name: "xx",
+        image: "/items/-armor/orbT1.webp",
+        tier: 1
+    }, {
+        name: "Orb of Corruption",
+        image: "/items/-armor/orbT2.webp",
+        tier: 2
+    }, {
+        name: "- Armour Aura",
+        image: "/items/-armor/armorAuraT2.png",
+        tier: 2
+    },
+    {
+        name: "Phoenix Egg",
+        image: "/items/-armor/armorAuraT3.webp",
+        tier: 3
+    },
+    // summon auras
+    {
+        name: "xx",
+        image: "/items/summonAuras/summonAuraT2.png",
+        tier: 2
+    }, {
+        name: "Scroll of the Unholy Legion",
+        image: "/items/summonAuras/scrollOfUnholyLegion.webp",
+        tier: 3
+    },
+    // attack summon
+    {
+        name: "xx",
+        image: "/items/atkSummon/atkSummonT2.webp",
+        tier: 2
+    }, {
+        name: "Hydra Scales",
+        image: "/items/atkSummon/atkSummonT3.webp",
+        tier: 3
+    },
+    // summon duration
+    {
+        name: "xx",
+        image: "/items/summonDuration/summonDurationT2.png",
+        tier: 2
+    }, {
+        name: "Skullshield",
+        image: "/items/summonDuration/summonDurationT3.webp",
+        tier: 3
+    },
+    // reset
+    {
+        name: "xx",
+        image: "/items/reset/resetT2.webp",
+        tier: 2
+    }, {
+        name: "xx",
+        image: "/items/reset/resetT3.webp",
+        tier: 3
+    },
+    // spelldmg
+    {
+        name: "xx",
+        image: "/items/spelldmg/spelldmgT2.webp",
+        tier: 2
+    }, {
+        name: "Staff of Sanctuary",
+        image: "/items/spelldmg/spelldmgT3.webp",
+        tier: 3
+    },
+    // t1 orbs
+    {
+        name: "xx",
+        image: "/items/orbs/orbRed.webp",
+        tier: 1
+    }, {
+        name: "xx",
+        image: "/items/orbs/orbGreen.webp",
+        tier: 1
+    }, {
+        name: "xx",
+        image: "/items/orbs/orbBlue.webp",
+        tier: 1
+    },
+    // stun
+    {
+        name: "xx",
+        image: "/items/stun/stunT2.webp",
+        tier: 2
+    }, {
+        name: "xx",
+        image: "/items/stun/stunT3.webp",
+        tier: 3
+    },
+    // general
     {
         name: "Gold Coins",
         image: "/items/goldCoin.webp",
@@ -9,24 +117,109 @@ export const Items: ItemEntity[] = [
         name: "Mindstaff",
         image: "/items/mindStaff.webp",
         tier: 2
-    },
-    {
-        name: "- Armour Aura",
-        image: "/items/armorAuraT2.png",
+    }, {
+        name: "xx",
+        image: "/items/invisDetection.webp",
         tier: 2
-    },
-    {
-        name: "Phoenix Egg",
-        image: "/items/armorAuraT3.webp",
+    }, {
+        name: "Shaman Claws",
+        image: "/items/shamanClaws.png",
+        tier: 3
+    }, {
+        name: "Bloodfeather Heart",
+        image: "/items/bloodfeatherHeart.webp",
+        tier: 3
+    }, {
+        name: "StaffOfSilence",
+        image: "/items/staffOfSilence.webp",
+        tier: 3
+    }, {
+        name: "xx",
+        image: "/items/fieryBow.webp",
+        tier: 3
+    }, {
+        name: "xx",
+        image: "/items/stormHammer.webp",
+        tier: 3
+    }, {
+        name: "xx",
+        image: "/items/stunResistT3.webp",
+        tier: 3
+    }, {
+        name: "Chitinous Blade",
+        image: "/items/chitBlade.png",
+        tier: 3
+    }, {
+        name: "xx",
+        image: "/items/agiT2.webp",
+        tier: 2
+    }, {
+        name: "xx",
+        image: "/items/agiT3.webp",
+        tier: 3
+    }, {
+        name: "xx",
+        image: "/items/axeT3.webp",
         tier: 3
     },{
-        name: "Orb of Corruption",
-        image: "/items/orbT2.webp",
-        tier: 2
-    },{
-        name: "OrbT1",
-        image: "/items/orbT1.webp",
+        name: "xx",
+        image: "/items/xxT3.webp",
+        tier: 3
+    }, {
+        name: "Ank",
+        image: "/items/ank.webp",
+        tier: 3
+    },
+
+    // Potions
+    {
+        name: "xx",
+        image: "/items/potions/rgbPot.webp",
         tier: 1
-    }
-    // Add more items here...
+    },
+    {
+        name: "xx",
+        image: "/items/potions/rrbPot.webp",
+        tier: 1
+    },
+    {
+        name: "xx",
+        image: "/items/potions/rrgPot.webp",
+        tier: 1
+    },
+    {
+        name: "xx",
+        image: "/items/potions/rrrPot.webp",
+        tier: 1
+    },
+    {
+        name: "xx",
+        image: "/items/potions/ggrPot.webp",
+        tier: 1
+    },
+    {
+        name: "xx",
+        image: "/items/potions/ggbPot.webp",
+        tier: 1
+    },
+    {
+        name: "xx",
+        image: "/items/potions/gggPot.webp",
+        tier: 1
+    },
+    {
+        name: "xx",
+        image: "/items/potions/bbrPot.webp",
+        tier: 1
+    },
+    {
+        name: "xx",
+        image: "/items/potions/bbgPot.webp",
+        tier: 1
+    },
+    {
+        name: "xx",
+        image: "/items/potions/bbbPot.webp",
+        tier: 1
+    },
 ];
